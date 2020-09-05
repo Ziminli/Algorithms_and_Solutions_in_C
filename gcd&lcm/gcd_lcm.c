c07a4f5836418326e36560784eb690e52c2e70f3
Ziminli <70735843+Ziminli@users.noreply.github.com>
2020-09-05 00:00:36 -0400

    Add files via upload

---
+++ gcd&lcm/gcd_lcm.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int gcd(int a, int b);
+int lcm(int a, int b);
+
+int main()
+{
+    int n, m, g, l;
+    
+    printf("Please enter the two integers which you want to find the HCF of:\n");
+    
+    printf("First number: ");
+    scanf("%d", &n);
+    printf("Second number: ");
+    scanf("%d", &m);
+    
+    g = gcd(n, m);
+    printf("The HCF of %d and %d is %d\n", n, m, g);
+    l = lcm(n, m);
+    printf("The LCM of %d and %d is %d\n", n, m, l);
+
+    system("pause");
+    return 0;
+}
+
+int gcd(int a, int b) 
+{
+    int r = a % b;
+    
+    while(r != 0)
+    {
+        a = b;
+        b = r;
+        r = a % b;
+    }
+    
+    return b;
+}
+
+int lcm(int a, int b)
+{   
+    return(a * b / gcd(a, b));
+}
